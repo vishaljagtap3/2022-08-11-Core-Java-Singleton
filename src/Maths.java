f31684615484352abf1b5b119fd3be584fe7c794
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-09-12 12:40:02 +0530

    Added mul to Maths class

--- src/Maths.java
+++ src/Maths.java
@@ -20,4 +20,8 @@ public class Maths {
     public int sub(int num1, int num2) {
         return num1 - num2;
     }
+
+    public int mul(int num1, int num2) {
+        return num1 * num2;
+    }
 }
